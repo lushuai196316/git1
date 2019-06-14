7731c712673161a8f40daa6b07c117b02a6cf96c
lushuai196316 <2575684300@qq.com>
2019-06-14 20:22:39 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/app/App.java
@@ -0,0 +1,11 @@
+package com.qfedu.app;
+
+/**
+ * Created by 鲁帅 on 2019/6/14.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hahahahhaah");
+    }
+}
